3d467ed9bf34f3e651a810756bcc3c3dca1e3d45
MiguelZea12 <aroryt02@gmail.com>
2023-07-04 17:33:18 -0500

    cambioos

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mi Proyecto Flutter',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
-        
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
       },
     );
   }
@@ -29,6 +30,8 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bienvenido a CAJA'),
+        backgroundColor: Colors.white, // Fondo blanco para el AppBar
+        foregroundColor: Colors.black, // Texto negro para el AppBar
       ),
       body: Center(
         child: Column(
@@ -55,7 +58,61 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black), // Icono en color negro
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon:
+                Icon(Icons.search, color: Colors.black), // Icono en color negro
+            label: 'Buscar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart,
+                color: Colors.black), // Icono en color negro
+            label: 'Carrito',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite,
+                color: Colors.black), // Icono en color negro
+            label: 'Favoritos',
+          ),
+        ],
+      ),
     );
   }
 }
 
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Iniciar sesión'),
+        backgroundColor: Colors.white, // Fondo blanco para el AppBar
+        foregroundColor: Colors.black, // Texto negro para el AppBar
+      ),
+      body: Center(
+        child: Text('Página de inicio de sesión'),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registrarse'),
+        backgroundColor: Colors.white, // Fondo blanco para el AppBar
+        foregroundColor: Colors.black, // Texto negro para el AppBar
+      ),
+      body: Center(
+        child: Text('Página de registro'),
+      ),
+    );
+  }
+}
