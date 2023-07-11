3301f8bca618c8ad43af889a3be60b7d65bdefde
Aldair2003 <luis.toala.r@gmail.com>
2023-07-11 13:40:23 -0500

    Cambio de login y register

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tu App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: HomePage(),
     );
@@ -22,26 +22,58 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Hermanas Tierra'),
+        leading: Padding(
+          padding: EdgeInsets.only(left: 16.0),
+          child: Image.asset(
+            'assets/IDENTIDAD - simbolo transparente_Mesa de trabajo 1.png',
+            width: 40,
+            height: 40,
+          ),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
+            TextButton.icon(
               onPressed: () {
                 // Acción para el botón de inicio de sesión
                 print('Iniciar sesión');
               },
-              child: Text('Iniciar sesión'),
+              icon: Icon(Icons.login, color: Colors.purple),
+              label: Text(
+                'Iniciar sesión',
+                style: TextStyle(fontSize: 18),
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.purple,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                side: BorderSide(color: Colors.purple),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
             ),
             SizedBox(height: 16),
-            ElevatedButton(
+            TextButton.icon(
               onPressed: () {
                 // Acción para el botón de registro
                 print('Registrarse');
               },
-              child: Text('Registrarse'),
+              icon: Icon(Icons.person_add, color: Colors.black),
+              label: Text(
+                'Registrarse',
+                style: TextStyle(fontSize: 18),
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                side: BorderSide(color: Colors.black),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
             ),
           ],
         ),
